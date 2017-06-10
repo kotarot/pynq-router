5872e78e5f70c07381401dc2bde22f2a7e0fd336
Kotaro Terada <tera@toki.waseda.jp>
2017-06-10 12:08:09 +0900

    Update LFSR etc.

--- sw/main.cpp
+++ sw/main.cpp
@@ -7,7 +7,6 @@
 #ifndef SOFTWARE
 #include <stdio.h>
 #include <string.h>
-//#include <ap_int.h>
 #endif
 
 #ifdef SOFTWARE
@@ -17,37 +16,60 @@
 #endif
 
 #include "./main.hpp"
-//#include "./route.hpp"
 
 
+#ifdef USE_MT
 // ================================ //
 // メルセンヌ・ツイスタ
 // ================================ //
 #include "mt19937ar.hpp"
 
 void mt_init_genrand(unsigned long s) {
-////#pragma HLS INLINE
+#pragma HLS INLINE
     init_genrand(s);
 }
 
-// AからBの範囲の整数の乱数が欲しいとき
+// AからBの範囲 (AとBを含む) の整数の乱数が欲しいとき
 // 参考 http://www.sat.t.u-tokyo.ac.jp/~omi/random_variables_generation.html
 unsigned long mt_genrand_int32(int a, int b) {
-////#pragma HLS INLINE
+#pragma HLS INLINE
     return genrand_int32() % (b - a + 1) + a;
 }
 
+
+#else
 // ================================ //
-// 探索
+// LFST
 // ================================ //
 
-void search(ap_uint<7> size_x, ap_uint<7> size_y, ap_uint<3> size_z,
-    ap_uint<17> start, ap_uint<17> goal, ap_uint<8> *w) {
+// 参考 https://highlevel-synthesis.com/2017/02/10/lfsr-in-hls/
+static ap_uint<32> lfsr;
 
-    cout << size_x << " " << size_y << " " << size_z << endl;
-    cout << start << " -> " << goal << endl;
-
+void lfsr_random_init(ap_uint<32> seed) {
+#pragma HLS INLINE
+    lfsr = seed;
 }
+
+ap_uint<32> lfsr_random() {
+#pragma HLS INLINE
+    bool b_32 = lfsr.get_bit(32-32);
+    bool b_22 = lfsr.get_bit(32-22);
+    bool b_2 = lfsr.get_bit(32-2);
+    bool b_1 = lfsr.get_bit(32-1);
+    bool new_bit = b_32 ^ b_22 ^ b_2 ^ b_1;
+    lfsr = lfsr >> 1;
+    lfsr.set_bit(31, new_bit);
+
+    return lfsr.to_uint();
+}
+
+// AからBの範囲 (AとBを含む) の整数の乱数が欲しいとき
+// 参考 http://www.sat.t.u-tokyo.ac.jp/~omi/random_variables_generation.html
+ap_uint<32> lfsr_random_uint32(ap_uint<32> a, ap_uint<32> b) {
+#pragma HLS INLINE
+    return lfsr_random() % (b - a + 1) + a;
+}
+#endif
 
 
 // ================================ //
@@ -66,13 +88,16 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
     ap_uint<7> size_y; // ボードの Y サイズ
     ap_uint<3> size_z; // ボードの Z サイズ
 
-    ap_uint<8> line_size = 0; // ラインの総数
+    ap_uint<8> line_num = 0; // ラインの総数
 
     ap_uint<17> starts[MAX_LINES]; // ラインのスタートリスト
+//#pragma HLS ARRAY_PARTITION variable=starts complete dim=0
     ap_uint<17> goals[MAX_LINES];  // ラインのゴールリスト
+//#pragma HLS ARRAY_PARTITION variable=goals complete dim=0
 
     ap_uint<8> weights[MAX_CELLS];          // セルの重み
     ap_uint<8> paths_size[MAX_LINES];       // ラインが対応するセルIDのサイズ
+//#pragma HLS ARRAY_PARTITION variable=paths_size complete dim=0
     ap_uint<17> paths[MAX_LINES][MAX_PATH]; // ラインが対応するセルIDの集合 (スタートとゴールは除く)
     bool adjacents[MAX_LINES];              // スタートとゴールが隣接しているライン
 //#pragma HLS ARRAY_PARTITION variable=adjacents complete dim=0
@@ -83,13 +108,13 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
 
     // ループカウンタは1ビット余分に用意しないと終了判定できない
     for (ap_uint<8> i = 0; i < (ap_uint<8>)(MAX_LINES); i++) {
-//#pragma HLS PIPELINE
+//#pragma HLS UNROLL
         adjacents[i] = false;
     }
 
     // ボードストリングの解釈
-    // unsigned 15bit は 32768 に収まるため
-    for (ap_uint<15> idx = 0; ; ) {
+    for (ap_uint<16> idx = 0; idx < BOARDSTR_SIZE; ) {
+//#pragma HLS PIPELINE
 #pragma HLS LOOP_TRIPCOUNT min=100 max=32768 avg=1000
 
         if (boardstr[idx] == 'X') {
@@ -105,20 +130,19 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
             idx += 2;
         }
         else if (boardstr[idx] == 'L') {
-            //ap_uint<8> i = (boardstr[idx+1] - '0') * 10 + (boardstr[idx+2] - '0');
             ap_uint<7> s_x = (boardstr[idx+1] - '0') * 10 + (boardstr[idx+2] - '0');
             ap_uint<7> s_y = (boardstr[idx+3] - '0') * 10 + (boardstr[idx+4] - '0');
             ap_uint<3> s_z = (boardstr[idx+5] - '0') - 1;
             ap_uint<7> g_x = (boardstr[idx+6] - '0') * 10 + (boardstr[idx+7] - '0');
             ap_uint<7> g_y = (boardstr[idx+8] - '0') * 10 + (boardstr[idx+9] - '0');
             ap_uint<3> g_z = (boardstr[idx+10] - '0') - 1;
-            //cout << "L " << line_size << ": " << s_x << " " << s_y << " " << s_z << " "
-            //                                  << g_x << " " << g_y << " " << g_z << endl;
+            //cout << "L " << line_num << ": " << s_x << " " << s_y << " " << s_z << " "
+            //                                 << g_x << " " << g_y << " " << g_z << endl;
             idx += 11;
 
             // スタートとゴール
-            starts[line_size] = (s_z << (BITWIDTH_X + BITWIDTH_Y)) & (s_y << BITWIDTH_X) & s_x;
-            goals[line_size]  = (g_z << (BITWIDTH_X + BITWIDTH_Y)) & (g_y << BITWIDTH_X) & g_x;
+            starts[line_num] = (s_z << (BITWIDTH_X + BITWIDTH_Y)) & (s_y << BITWIDTH_X) & s_x;
+            goals[line_num]  = (g_z << (BITWIDTH_X + BITWIDTH_Y)) & (g_y << BITWIDTH_X) & g_x;
 
             // 初期状態で数字が隣接しているか判断
             ap_int<8> dx = g_x - s_x;
@@ -126,12 +150,12 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
             ap_int<4> dz = g_z - s_z;
             if ((dx == 0 && dy == 0 && (dz == 1 || dz == -1)) || (dx == 0 && (dy == 1 || dy == -1) && dz == 0) ||
                 ((dx == 1 || dx == -1) && dy == 0 && dz == 0)) {
-                adjacents[line_size] = true;
+                adjacents[line_num] = true;
             } else {
-                adjacents[line_size] = false;
+                adjacents[line_num] = false;
             }
 
-            line_size++;
+            line_num++;
         }
 
         // 終端 (null) 文字
@@ -152,7 +176,11 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
     }
 
     // 乱数の初期化
+#if USE_MT
     mt_init_genrand(12345);
+#else
+    lfsr_random_init(12345);
+#endif
 
     // ================================
     // 初期化 END
@@ -163,7 +191,8 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
     ap_int<8> output;
 
     // 初期ルーティング
-    for (ap_uint<8> i = 0; i < (ap_uint<8>)(line_size); i++) {
+    for (ap_uint<8> i = 0; i < (ap_uint<8>)(line_num); i++) {
+//#pragma HLS UNROLL
 #pragma HLS LOOP_TRIPCOUNT min=2 max=128 avg=50
 
         // 数字が隣接する場合スキップ
@@ -171,8 +200,15 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
             continue;
         }
 
+        // 乱数テスト
+        //for (int a = 0; a < 100; a++)
+        //    cout << static_cast<std::bitset<32> >(lfsr_random()) << endl;
+        //for (int a = 0; a < 100; a++)
+        //    cout << lfsr_random_uint32(10, 100) << endl;
+
         // ダイクストラ法
         search(size_x, size_y, size_z, starts[i], goals[i], weights);
+
     }
 
     // 解導出できなかった場合
@@ -183,4 +219,16 @@ bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status) {
 
     *status = 0;
     return true;
+}
+
+
+// ================================ //
+// 探索
+// ================================ //
+
+void search(ap_uint<7> size_x, ap_uint<7> size_y, ap_uint<3> size_z, ap_uint<17> start, ap_uint<17> goal, ap_uint<8> w[]) {
+
+    cout << size_x << " " << size_y << " " << size_z << endl;
+    cout << start << " -> " << goal << endl;
+
 }
--- sw/main.hpp
+++ sw/main.hpp
@@ -8,12 +8,7 @@
 #define __MAIN_HPP__
 
 #ifdef SOFTWARE
-#include "ap_int.h"
-#else
-#include <ap_int.h>
-#endif
-
-#ifdef SOFTWARE
+#include <bitset>
 #include <iomanip>
 #include <iostream>
 #include <fstream>
@@ -27,11 +22,23 @@
 #include <time.h>
 #endif
 
+#ifdef SOFTWARE
+#include "ap_int.h"
+#else
+#include <ap_int.h>
+#endif
+
+
 using namespace std;
 
-#define NOT_USE -1
+
+// メルセンヌ・ツイスタを使う場合
+//#define USE_MT 1
+
 
 // 各種設定値
+#define NOT_USE -1
+
 #define MAX_BOXES 72     // X, Y の最大値 (7ビットで収まる)
 #define BITWIDTH_X 7
 #define BITWIDTH_Y 7
@@ -44,14 +51,22 @@ using namespace std;
 #define BOARDSTR_SIZE 32768 // ボードストリングの最大文字数 (15ビットで収まる : 値はとりあえず)
 
 
+#ifdef USE_MT
 // メルセンヌ・ツイスタ
 void mt_init_genrand(unsigned long s);
 unsigned long mt_genrand_int32(int a, int b);
+#else
+void lfsr_random_init(ap_uint<32> seed);
+ap_uint<32> lfsr_random();
+ap_uint<32> lfsr_random_uint32(ap_uint<32> a, ap_uint<32> b);
+#endif
 
 bool pynqrouter(char boardstr[BOARDSTR_SIZE], ap_int<8> *status);
 //void initialize(char boardstr[BOARDSTR_SIZE], Board *board);
 //bool isFinished(Board *board);
 //void generateSolution(char boardstr[BOARDSTR_SIZE], Board *board);
+
+void search(ap_uint<7> size_x, ap_uint<7> size_y, ap_uint<3> size_z, ap_uint<17> start, ap_uint<17> goal, ap_uint<8> w[]);
 
 
 #endif /* __MAIN_HPP__ */
