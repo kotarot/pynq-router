22c16015f761e7643249154a8eb1bdaf62fd0c06
Kotaro Terada <tera@toki.waseda.jp>
2017-08-20 02:02:48 +0900

    Update solve function in solver script

--- solver/pynqrouter.py
+++ solver/pynqrouter.py
@@ -29,6 +29,102 @@ BITWIDTH_Z = 3
 IP_LED = 'SEG_axi_gpio_0_Reg'
 
 
+def solve(boardstr, seed=12345, zero_padding=False):
+    print('boardstr:')
+    print(boardstr)
+    print('seed:')
+    print(seed)
+    print('')
+
+    # ボード文字列から X, Y, Z を読んでくる
+    size_x = (ord(boardstr[1]) - ord('0')) * 10 + (ord(boardstr[2]) - ord('0'))
+    size_y = (ord(boardstr[4]) - ord('0')) * 10 + (ord(boardstr[5]) - ord('0'))
+    size_z = (ord(boardstr[7]) - ord('0'))
+
+    # Overlay 読み込み
+    OL = Overlay('pynqrouter.bit')
+    OL.download()
+    print(OL.ip_dict)
+    print('Overlay loaded!')
+
+    # MMIO 接続 (pynqrouter)
+    mmio = MMIO(int(PL.ip_dict[IP][0]), int(PL.ip_dict[IP][1]))
+
+    # MMIO 接続 & リセット (LED)
+    mmio_led = MMIO(int(PL.ip_dict[IP_LED][0]), int(PL.ip_dict[IP_LED][1]))
+    mmio_led.write(0, 0)
+
+    # 入力データをセット
+    imem = pack(boardstr)
+    for i in range(len(imem)):
+        mmio.write(OFFSET_BOARD + (i * 4), imem[i])
+
+    mmio.write(OFFSET_SEED, seed)
+
+    # スタート
+    # ap_start (0 番地の 1 ビット目 = 1)
+    mmio.write(0, 1)
+    print('Start!')
+    time_start = time.time()
+
+    # ap_done (0 番地の 2 ビット目 = 2) が立つまで待ってもいいが
+    #   done は一瞬だけ立つだけのことがあるから
+    # ap_idle (0 番地の 3 ビット目 = 4) を待ったほうが良い
+    iteration = 0
+    while (mmio.read(0) & 4) == 0:
+        # 動いてるっぽく見えるようにLチカさせる
+        iteration += 1
+        if iteration == 10000:
+            mmio_led.write(0, 3)
+        elif 20000 <= iteration:
+            mmio_led.write(0, 12)
+            iteration = 0
+
+    # 完了の確認
+    print('Done!')
+    print('control:', mmio.read(0))
+    time_done = time.time()
+    elapsed = time_done - time_start
+    print('elapsed:', elapsed)
+    print('')
+
+    # 状態の取得
+    status = int(mmio.read(OFFSET_STATUS))
+    print('status:', status)
+    if status != 0:
+        # 解けなかったらLEDを消す
+        mmio_led.write(0, 0)
+        sys.stderr.write('Cannot solve it!\n')
+        return { 'solved': False, 'solution': '' }
+    print('Solved!')
+
+    # 解けたらLEDを全部つける
+    mmio_led.write(0, 15)
+
+    # 出力
+    omem = []
+    for i in range(len(imem)):
+        omem.append(mmio.read(OFFSET_BOARD + (i * 4)))
+    boards = unpack(omem)
+
+    # 回答の生成
+    solution = ('SIZE ' + str(size_x) + 'X' + str(size_y) + 'X' + str(size_z) + '\n')
+    for z in range(size_z):
+        solution += ('LAYER ' + str(z + 1) + '\n')
+        for y in range(size_y):
+            for x in range(size_x):
+                if x != 0:
+                    solution += ','
+                i = ((x * MAX_X + y) << BITWIDTH_Z) | z
+                if zero_padding:
+                    solution += '{0:0>2}'.format(boards[i])  # 2桁の0詰め
+                else:
+                    solution += str(boards[i])               # 普通に表示
+            solution += '\n'
+
+    return { 'solved': True, 'solution': solution }
+
+
 def main():
     parser = argparse.ArgumentParser(description="Solver with pynqrouter")
 
@@ -69,109 +165,19 @@ def main():
         sys.stderr.write('Specify at least "input" or "boardstr"!\n')
         sys.exit(1)
 
-    print('boardstr:')
-    print(boardstr)
-    print('terminals:')
-    print(args.terminals)
-    print('seed:')
-    print(args.seed)
-    print('')
-
-    # ボード文字列から X, Y, Z を読んでくる
-    size_x = (ord(boardstr[1]) - ord('0')) * 10 + (ord(boardstr[2]) - ord('0'))
-    size_y = (ord(boardstr[4]) - ord('0')) * 10 + (ord(boardstr[5]) - ord('0'))
-    size_z = (ord(boardstr[7]) - ord('0'))
-
-    # Overlay 読み込み
-    OL = Overlay('pynqrouter.bit')
-    OL.download()
-    print(OL.ip_dict)
-    print('Overlay loaded!')
-
-    # MMIO 接続 (pynqrouter)
-    mmio = MMIO(int(PL.ip_dict[IP][0]), int(PL.ip_dict[IP][1]))
-
-    # MMIO 接続 & リセット (LED)
-    mmio_led = MMIO(int(PL.ip_dict[IP_LED][0]), int(PL.ip_dict[IP_LED][1]))
-    mmio_led.write(0, 0)
-
-    # 入力データをセット
-    imem = pack(boardstr)
-    for i in range(len(imem)):
-        mmio.write(OFFSET_BOARD + (i * 4), imem[i])
-
-    mmio.write(OFFSET_SEED, args.seed)
-
-    # スタート
-    # ap_start (0 番地の 1 ビット目 = 1)
-    mmio.write(0, 1)
-    print('Start!')
-    time_start = time.time()
-
-    # ap_done (0 番地の 2 ビット目 = 2) が立つまで待ってもいいが
-    #   done は一瞬だけ立つだけのことがあるから
-    # ap_idle (0 番地の 3 ビット目 = 4) を待ったほうが良い
-    iteration = 0
-    while (mmio.read(0) & 4) == 0:
-        # 動いてるっぽく見えるようにLチカさせる
-        iteration += 1
-        if iteration == 10000:
-            mmio_led.write(0, 3)
-        elif 20000 <= iteration:
-            mmio_led.write(0, 12)
-            iteration = 0
-
-    # 完了の確認
-    print('Done!')
-    print('control:', mmio.read(0))
-    time_done = time.time()
-    elapsed = time_done - time_start
-    print('elapsed:', elapsed)
-    print('')
-
-    # 状態の取得
-    status = int(mmio.read(OFFSET_STATUS))
-    print('status:', status)
-    if status != 0:
-        # 解けなかったらLEDを消す
-        mmio_led.write(0, 0)
-        sys.stderr.write('Cannot solve it!\n')
+    result = solve(boardstr, args.seed)
+    if result['solved'] == False:
         sys.exit(1)
-    print('Solved!')
-
-    # 解けたらLEDを全部つける
-    mmio_led.write(0, 15)
-
-    # 出力
-    omem = []
-    for i in range(len(imem)):
-        omem.append(mmio.read(OFFSET_BOARD + (i * 4)))
-    boards = unpack(omem)
-
-    # 回答の生成
-    solution = ('SIZE ' + str(size_x) + 'X' + str(size_y) + 'X' + str(size_z) + '\n')
-    for z in range(size_z):
-        solution += ('LAYER ' + str(z + 1) + '\n')
-        for y in range(size_y):
-            for x in range(size_x):
-                if x != 0:
-                    solution += ','
-                i = ((x * MAX_X + y) << BITWIDTH_Z) | z
-                if args.zero_padding:
-                    solution += '{0:0>2}'.format(boards[i])  # 2桁の0詰め
-                else:
-                    solution += str(boards[i])               # 普通に表示
-            solution += '\n'
 
     # 表示 & ファイル出力
     if args.print:
         print('')
         print('SOLUTION')
         print('========')
-        print(solution)
+        print(result['solution'])
     if args.output is not None:
         with open(args.output, 'w') as f:
-            f.write(solution)
+            f.write(result['solution'])
 
 
 def pack(_str):
